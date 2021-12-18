9dafc0007ed8a16e9dd1a8e1d1d2ea429ebed052
safari47 <96324189+safari47@users.noreply.github.com>
2021-12-18 13:15:29 +1000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace TestConsole   
+{
+    class Programm
+    {
+        static void Main (string[] args)
+        {
+Random rnd=new Random();
+ int [] num = new int[10];
+ int length=num.Length-1;
+for (int i=0;i<length+1;i++)
+{num[i]=rnd.Next(0,100);
+Console.WriteLine(num[i]);
+}
+Array.Sort(num);
+for (int j=0;j<length+1;j++)
+{
+Console.WriteLine(num[j]);
+}
+Console.WriteLine ("Это самые большие числа из массива "+num[length]+ ' ' +num[length-1]);
+        }
+    }
+}
